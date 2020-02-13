03f94cf489bb00912959d14c8b4cfd452f205d42
Pam Deininger <pameladeininger@gmail.com>
2020-02-12 21:03:18 -0500

    additions

--- server.js
+++ server.js
@@ -1,1 +1,22 @@
 var express = require(express);
+var exphbs = require("express-handlebars");
+
+var PORT = process.env.PORT || 8080;
+
+var app = express();
+app.use(express.urlencoded({ extended: true}));
+app.use(express.json());
+
+//handlebars
+var exphbs = require("express-handlebars");
+app.engine("handlebars", exphbs({ defaultLayout:  "main" }));
+app.set("view engine", "handlebars");
+
+//Import routes with server access
+var routes = require();
+
+app.use(routes);
+
+app.listen(PORT, function() {
+    console.log("Server listening on:  http://localhost:" + PORT);
+});
