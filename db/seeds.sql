0f5e173edf8ff16f5468eeb80f1ebc95c734cf36
Pam Deininger <pameladeininger@gmail.com>
2020-02-18 18:16:06 -0500

    etc

--- db/seeds.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO burger (burger_type, devoured) VALUES ('Cheeseburger', true);
-INSERT INTO burger (burger_type, devoured) VALUES ('Veggieburger', true);
-INSERT INTO burger (burger_type, devoured) VALUES ('Double cheeseburger', true);
-INSERT INTO burger (burger_type, devoured) VALUES ('Baconburger', true);
-INSERT INTO burger (burger_type, devoured) VALUES ('Plantburger', true);
